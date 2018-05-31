03d68414dcc7a155ad868c07a5208339c067dd2b
Dinh Le Khanh Duy <ga67qac@mytum.de>
2018-05-31 20:21:16 +0200

    Change CORS

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ const jsonWebToken    = require('jsonwebtoken');
 const allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
