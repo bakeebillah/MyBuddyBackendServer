8045ea8fbce97b919cb8578760355f11fe355c96
Newaz Bakee Billah <bakee.billah@tum.de>
2018-06-14 01:56:24 +0200

    Revert "Revert "Revert "Merge Conflicts removed"""
    This reverts commit c7e4c1514ae00c05efe375c58f24d0f7a05a3667.

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const userModel =  require('../models/userModel');
+const userModel =  require('../models/user');
 const jsonWebToken = require('jsonwebtoken');
 const config = require('../configaration');
 const bcrypt = require('bcrypt');
@@ -8,54 +8,64 @@ const bcrypt = require('bcrypt');
 
 const register =(req,res)=>{
     if (req.body.password !== req.body.passwordConf) {
-        return res.status(400).json({
-         error:'Bad Request' ,
-            message:'Passwords do not match.'
-        });
-    }
-    if (req.body.email &&
-        req.body.username &&
-        req.body.password &&
-        req.body.passwordConf) {
 
-        const user = Object.assign(req.body, {password: bcrypt.hashSync(req.body.password, 8)});
-        //use schema.create to insert data into the db
-        userModel.create(user)
-            .then(user => {
-                // if user is registered without errors
-                // create a token
-                const token = jsonWebToken.sign({ id: user._id, username: user.username }, config.JwtSecret, {
-                    expiresIn: 86400 // expires in 24 hours
-                });
-                res.status(200).json({
-                    token: token,
-                    message: "Successfully created your account."
-                });
-            })
-            .catch(error => {
-                if(error.code == 11000) {
-                    res.status(400).json({
-                        error: 'User exists',
-                        message: error.message
-                    })
-                }
-                else{
-                    res.status(500).json({
-                        error: 'Internal server error',
-                        message: error.message
-                    })
-                }
+    return res.status(400).json({
+     error:'Bad Request' ,
+        message:'Passwords do not match.'
+    });
+
+}
+
+if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+
+    const user = Object.assign(req.body, {password: bcrypt.hashSync(req.body.password, 8)});
+    //use schema.create to insert data into the db
+    userModel.create(user)
+        .then(user => {
+
+            // if user is registered without errors
+            // create a token
+            const token = jsonWebToken.sign({ id: user._id, username: user.username }, config.JwtSecret, {
+                expiresIn: 86400 // expires in 24 hours
             });
-    }// end if
-    else {
-        return res.status(400).json({
-            error:'Bad Request' ,
-            message:'All fields required.'
+
+            res.status(200).json({token: token});
+
+
+        })
+        .catch(error => {
+            if(error.code == 11000) {
+                res.status(400).json({
+                    error: 'User exists',
+                    message: error.message
+                })
+            }
+            else{
+                res.status(500).json({
+                    error: 'Internal server error',
+                    message: error.message
+                })
+            }
         });
-    } // end else
+
+}// end if
+else {
+
+    return res.status(400).json({
+        error:'Bad Request' ,
+        message:'All fields required.'
+    });
+} // end else
+
+
 };
 
 const login = (req,res)=>{
+
     // Checking blank username
     if(!req.body.username){
         return res.status(400).json({
@@ -74,6 +84,8 @@ const login = (req,res)=>{
 
     userModel.findOne({username: req.body.username}).exec()
         .then(user => {
+
+
             const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
             if (!isPasswordValid) return res.status(401).send({token: null });
 
@@ -82,15 +94,21 @@ const login = (req,res)=>{
             const token = jsonWebToken.sign({ id: user._id, username: user.username }, config.JwtSecret, {
                 expiresIn: 86400 // expires in 24 hours
             });
+
             res.status(200).json({token: token});
+
         })
         .catch(error => res.status(404).json({
             error: 'User Not Found',
             message: error.message
         }));
-};
+
+}
 
 const me = (req, res) => {
+
+
+ 
 
     var ObjectId = require('mongodb').ObjectId;
     var id = req.userId;
@@ -98,10 +116,13 @@ const me = (req, res) => {
 
     userModel.find({_id:o_id}).select('username').exec()
         .then(user => {
+
             if (!user) return res.status(404).json({
                 error: 'Not Found',
                 message: `User not found`
             });
+
+
             res.status(200).json(user)
         })
         .catch(error => res.status(500).json({
@@ -128,28 +149,6 @@ const statustest = (req, res) => {
     });
 };
 
-const messageback = (req, res) => {
-    console.log('Hello World');
-    console.log(res);
-    res.status(200).send({'message': 'Hello World'});
-}
-
-const getUser = (req, res) => {
-    if(!req.body.username){
-        return res.status(400).json({
-            error:'Bad Request',
-            message: 'The request must contain a username property'
-        });
-    } // end if - Checking blank username
-    userModel.findOne({username: req.body.username}).exec()
-        .then(user => {
-            res.sendStatus(200);
-        })
-        .catch(error => res.status(404).json({
-            error: 'User Not Found',
-            message: error.message
-        }));
-}
 
 
 module.exports = {
@@ -157,7 +156,6 @@ module.exports = {
     register,
     me,
     logout,
-    statustest,
-    messageback,
-    getUser
+    statustest
+
 };
--- src/middleware.js
+++ src/middleware.js
@@ -1,39 +1,49 @@
 "use strict";
 const config = require('./configaration');
 const jsonWebToken    = require('jsonwebtoken');
-const uuidv4 = require('uuid/v4')
+
 
 const allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
+
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-        res.sendStatus(200);
+        res.status(200).send(200);
     }
     else {
         next();
     }
 };
 
+
 const checkAuthentication = (req, res, next) => {
+
     // check header or url parameters or post parameters for token
     const token = req.headers['x-access-token'];
+
     if (!token)
         return res.status(401).send({
             error: 'Unauthorized',
             message: 'No token provided in the request'
         });
+
     // verifies secret and checks exp
     jsonWebToken.verify(token, config.JwtSecret, (err, decoded) => {
         if (err) return res.status(401).send({
             error: 'Unauthorized',
             message: 'Failed to authenticate token.'
         });
+
         // if everything is good, save to request for use in other routes
         req.userId = decoded.id;
+
+
         next();
     });
+
+
 };
 
 const errorHandler = (err, req, res, next) => {
@@ -44,31 +54,8 @@ const errorHandler = (err, req, res, next) => {
     res.render('error', { error: err })
 };
 
-const createChat = ({messages = [], name = "", users = []} = {}) => ({
-        id: uuidv4(),
-        name,
-        messages,
-        users,
-    }
-)
-
-const createMessage = ({message = "", sender = ""} = {}) => ({
-        id: uuidv4(),
-        time: getTime(new Date(Date.now())),
-        message,
-        sender
-    }
-)
-
-const getTime = (date)=>{
-    return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
-}
-
 module.exports = {
     allowCrossDomain,
     checkAuthentication,
-    errorHandler,
-    createChat,
-    createMessage
+    errorHandler
 };
-
