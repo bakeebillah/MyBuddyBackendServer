48bb254c8b1dcd32bb418aed4b4a4d4099bc9bdf
Newaz Bakee Billah <bakee.billah@tum.de>
2018-06-14 01:54:00 +0200

    Revert "Merge Conflicts removed"
    This reverts commit e6d757cbcd9d5471d335253990e643c4afdf8b0d, reversing
    changes made to b70549b4d01fda9ebf129ea5e46722a6719884f2.

--- mybuddy-bacnkend-server.js
+++ mybuddy-bacnkend-server.js
@@ -3,17 +3,14 @@ const http = require ('http');
 const mongoose   = require('mongoose');
 const api = require('./src/api');
 const config = require('./src/configaration');
-const socketController = require('./src/controllers/socketController');
 
 // set the port to the API
 api.set('port',config.port);
 
 // Create a http server
 const server = http.createServer(api);
-const io = module.exports.io = require('socket.io')(server);
 
-io.on('connection', socketController);
-io.listen(8000);
+
 
 mongoose
     .connect(config.mongoURI)
@@ -26,6 +23,7 @@ mongoose
 server.on('listening', () => {
 
   //  console.log(`${config.basicRouteMessage} is running in port ${config.port}`);
+
     console.log('\x1b[44m\n\n\t\t\t'+ config.basicRouteMessage + ' is running on port '+ config.port+'\x1b[0m');
 });
 
